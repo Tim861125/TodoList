4b551d21203bf56ea53e851a2985bf114afea19b
Tim861125 <tim861125@gmail.com>
2025-01-31 20:19:35 +0800

    upgrade

---
+++ my_react_todo/src/components/CreateForm.jsx
@@ -0,0 +1,26 @@
+import { useState } from "react";
+
+function CreateForm({ addTodo }) {
+  const [content, setContent] = useState("");
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    addTodo(content);
+    setContent("");
+  };
+  return (
+    <form className="create-form" onSubmit={handleSubmit}>
+      <input
+        type="text"
+        placeholder="Enter"
+        value={content}
+        onChange={(e) => {
+          setContent(e.target.value);
+        }}
+      />
+
+      <button type="submit"> Add </button>
+    </form>
+  );
+}
+
+export default CreateForm;
